7792311223b482cc8ca029ba943ac0e879e342ae
Alex <ag2719@ic.ac.uk>
2023-02-21 17:59:52 +0000

    Continue implementing TicketNFT.

--- src/contracts/TicketNFT.sol
+++ src/contracts/TicketNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.10;
 
 import "../contracts/PurchaseToken.sol";
 import "../interfaces/ITicketNFT.sol";
+import "../interfaces/IPrimaryMarket.sol";
 
 contract TicketNFT is ITicketNFT {
     struct TicketHolder {
@@ -10,74 +11,99 @@ contract TicketNFT is ITicketNFT {
         string name;
     }
 
-    string public override name;
-    string public override symbol;
+    string public name;
+    string public symbol;
 
     address public immutable minter;
     PurchaseToken public immutable paymentToken;
 
-    uint256 public immutable nftPrice;
-    uint256 public lastTokenId;
+    IPrimaryMarket public immutable primaryMarket;
+    uint256 public immutable validityDuration;
+
+    uint256 public nftPrice;
+    uint256 public lastTicketID;
 
     mapping(address => uint256) internal _balances;
     mapping(uint256 => TicketHolder) internal _owners;
-    mapping(uint256 => uint256) internal _startTimes;
+    mapping(uint256 => address) internal _approvals;
+    mapping(uint256 => bool) internal _used;
+    mapping(uint256 => uint256) internal _expiryTimes;
 
     constructor(
         string memory name_,
         string memory symbol_,
-        PurchaseToken _paymentToken
+        PurchaseToken _paymentToken,
+        uint256 _initialNftPrice,
+        IPrimaryMarket primaryMarket_
     ) {
         name = name_;
         symbol = symbol_;
         minter = msg.sender;
         paymentToken = _paymentToken;
-        nftPrice = 1 * 100**18;
+        primaryMarket = primaryMarket_;
+        validityDuration = 864000; // 10 days.
+        nftPrice = _initialNftPrice;
+        lastTicketID = 0;
     }
 
-    function balanceOf(address account)
+    function mint(address holder, string memory holderName) external {
+        require(
+            msg.sender == address(primaryMarket),
+            "Only the primary market can mint tickets"
+        );
+
+        uint256 ticketID = lastTicketID + 1;
+        // paymentToken.transferFrom(msg.sender, address(0), nftPrice);
+
+        _balances[holder] += 1;
+        _owners[ticketID] = TicketHolder(holder, holderName);
+        _used[ticketID] = false;
+        _expiryTimes[ticketID] = block.timestamp + validityDuration;
+
+        lastTicketID = ticketID;
+        emit Transfer(address(0), holder, ticketID);
+    }
+
+    function balanceOf(address holder)
         external
         view
         override
         returns (uint256)
     {
-        return _balances[account];
+        return _balances[holder];
     }
 
-    function ownerOf(uint256 tokenId) external view returns (address) {
-        return _owners[tokenId].holder;
+    function ownerOf(uint256 ticketID) external view returns (address) {
+        require(lastTicketID >= ticketID, "Ticket does not exist");
+
+        return _owners[ticketID].holder;
     }
 
-    function mint(address holder, string memory holderName)
-        external
-    {
-        require(msg.sender == minter, "Only minter can mint");
+    function transferFrom(
+        address from,
+        address to,
+        uint256 ticketID
+    ) external {
+        address ticketHolder = _owners[ticketID].holder;
 
-        uint256 tokenId = lastTokenId + 1;
-        paymentToken.transferFrom(msg.sender, address(0), nftPrice);
+        require(to != address(0), "'to' address cannot be the zero address");
+        require(
+            from != address(0),
+            "'from' address cannot be the zero address"
+        );
+        require(
+            ticketHolder == from,
+            "'from' address is not the owner of this ticket"
+        );
 
-        _balances[holder] += 1;
-        _owners[tokenId] = TicketHolder(holder, holderName);
-        _startTimes[tokenId] = block.timestamp;
-
-        lastTokenId = tokenId;
-        emit Transfer(address(0), holder, tokenId);
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[ticketID].holder = to;
     }
 
+    function isExpiredOrUsed(uint256 ticketID) external view returns (bool) {
+        require(lastTicketID >= ticketID, "Ticket does not exist");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+        return _used[ticketID] || block.timestamp > _expiryTimes[ticketID];
+    }
 }
